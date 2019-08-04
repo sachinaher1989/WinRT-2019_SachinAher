a18a0ab44ddae9b7c67121a66074ae4aeffbb9c5
unknown <sachinaher1989@gmail.com>
2019-08-04 16:22:22 +0530

    Test

---
+++ Sachin/MyWindow/MyWindow/MyWindow.cpp
@@ -0,0 +1,9 @@
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpszCmdLine, int nCmdShow)
+{
+	WNDCLASSEX wndclass
+
+}
