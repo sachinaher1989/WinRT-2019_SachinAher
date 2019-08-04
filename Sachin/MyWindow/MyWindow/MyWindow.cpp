13a28922d1beea717dc01f04c71357dcbf9d02ab
unknown <sachinaher1989@gmail.com>
2019-08-04 17:31:39 +0530

    Assignment 1 :- MyWindow

--- Sachin/MyWindow/MyWindow/MyWindow.cpp
+++ Sachin/MyWindow/MyWindow/MyWindow.cpp
@@ -2,8 +2,60 @@
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpszCmdLine, int nCmdShow)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpszCmdLine, int iCmdShow)
 {
-	WNDCLASSEX wndclass
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MyApp");
+	
+	//code
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.hCursor  = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hInstance = hInstance;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
 
+	RegisterClassEx(&wndclass);
+
+	hwnd = CreateWindow(szAppName,
+		TEXT("MyApplication"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	// MessageLoop
+	while(GetMessage(&msg, NULL, 0,0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return((int)msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch(iMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
 }
